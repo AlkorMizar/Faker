c81dd7c03cf7ca7dda769d6d278110624c728ab8
AlkorMizar <anisa15012002@gmail.com>
2021-10-05 23:24:41 +0300

    ver 0.4

--- FakerAPI/API/Faker.cs
+++ FakerAPI/API/Faker.cs
@@ -13,79 +13,183 @@ namespace FakerAPI.API
         }
 
         private object Create(Type type) {
-            if (type.IsValueType)
+            
+            if (type.IsPrimitive)
             {
-
+                return CreateValueType(Type.GetTypeCode(type));
             }
-            else
+            else if (type.IsEnum) {
+                return CreateEnum(type);
+            }else
             {
+                object result;
                 ConstructorInfo[] constructorInfos = type.GetConstructors();
+                if (constructorInfos.Length != 0)
+                {
+                    ConstructorInfo maxParamConstructor = constructorInfos[0];
+                    ParameterInfo[] maxParamInfo = maxParamConstructor.GetParameters();
+                    foreach (var info in constructorInfos)
+                    {
+                        if (info.GetParameters().Length > maxParamInfo.Length)
+                        {
+                            maxParamConstructor = info;
+                            maxParamInfo = info.GetParameters();
+                        }
+                    }
+                    object[] parameters = new object[maxParamInfo.Length];
+                    for (int i = 0; i < maxParamInfo.Length; i++)
+                    {
+                        parameters[i] = Create(maxParamInfo[i].ParameterType);
+                    }
+                    result = Activator.CreateInstance(type, parameters);
+                }
+                else 
+                {
+                    result = Activator.CreateInstance(type);
+                }
+                PropertyInfo[] propertyInfo = type.GetProperties();
+                foreach (var property in propertyInfo)
+                {
+                    if (property.CanRead && property.GetValue(result)!=null && property.CanWrite) {
+                        property.SetValue(result,Create(property.PropertyType));
+                    }
+                }
+                FieldInfo[] fieldInfos = type.GetFields();
+                foreach (var field in fieldInfos)
+                {
+
+                    field.SetValue(result, Create(field.FieldType));
+                }
+
             }
+            return null;
         }
 
-        private void Create(out bool result)
+        private object CreateValueType(TypeCode type)
         {
-            result = rnd.Next() >= rnd.Next();
+            switch (type) {
+                case TypeCode.Boolean: {
+                     return CreateBoolean();
+                }
+                case TypeCode.Byte:
+                {
+                    return CreateByte();
+                }
+                case TypeCode.SByte:
+                {
+                    return CreateSbyte();
+                }
+                case TypeCode.Char:
+                {
+                    return CreateChar();
+                }
+                case TypeCode.Int16:
+                {
+                    return CreateShort();
+                }
+                case TypeCode.UInt16:
+                {
+                    return CreateUShort();
+                }
+                case TypeCode.Int32:
+                {
+                    return CreateInt();
+                }
+                case TypeCode.UInt32:
+                {
+                    return CreateUInt();
+                }
+                case TypeCode.Int64:
+                {
+                    return CreateLong();
+                }
+                case TypeCode.UInt64:
+                {
+                    return CreateULong();
+                }
+                case TypeCode.Single:
+                {
+                    return CreateFloat();
+                }
+                case TypeCode.Double:
+                {
+                    return CreateDouble();
+                }
+                case TypeCode.Decimal:
+                {
+                    return CreateDecimal();
+                }
+                default:
+                    return null;
+            }
+            
         }
 
-        private void Create(out byte result)
+        private bool CreateBoolean() {
+            return rnd.Next() > rnd.Next();
+        }
+        private byte CreateByte()
         {
-            result = (byte)rnd.Next();
+            return (byte)rnd.Next();
         }
 
-        private void Create(out sbyte result)
+        private sbyte CreateSbyte()
         {
-            result = (sbyte)rnd.Next();
+            return (sbyte)rnd.Next();
+        }
+        private char CreateChar()
+        {
+            return (char)rnd.Next();
         }
 
-        private void Create(out char result)
+        private short CreateShort()
         {
-            result = (char)rnd.Next();
+            return (short)rnd.Next();
         }
 
-        private void Create(out short result)
+        private ushort CreateUShort()
         {
-            result = (short)rnd.Next();
+            return (ushort)rnd.Next();
         }
 
-        private void Create(out ushort result)
-        {
-            result = (ushort)rnd.Next();
+        private int CreateInt() {
+            return rnd.Next();
         }
 
-        private void Create(out int result) {
-            result = rnd.Next();
+        private uint CreateUInt()
+        {
+            return (uint)rnd.Next();
         }
 
-        private void Create(out uint result)
+        private long CreateLong()
         {
-            result = (uint)rnd.Next();
+            return (long)rnd.Next() * (long)rnd.Next();
         }
 
-        private void Create(out long result)
+        private ulong CreateULong()
         {
-            result = rnd.Next();
-            result = result * rnd.Next();
+            return ((ulong)rnd.Next()) * ((ulong)rnd.Next());
         }
 
-        private void Create(out ulong result)
+        private float CreateFloat()
         {
-            result = ((ulong)rnd.Next());
-            result = (result * ((ulong)rnd.Next()));
+            return (float)rnd.NextDouble()*rnd.Next();
         }
 
-        private void Create(out float result)
+        private double CreateDouble()
         {
-            result =(float)rnd.NextDouble()*rnd.Next();
+            return rnd.NextDouble()*rnd.Next();
+        }
+        private decimal CreateDecimal()
+        {
+            return(decimal) rnd.NextDouble() / rnd.Next() * rnd.Next();
         }
 
-        private void Create(out double result)
-        {
-            result = rnd.NextDouble()*rnd.Next();
+        private object CreateEnum(Type type) {
+            string[] enumConst = Enum.GetNames(type);
+            int ind = rnd.Next() % enumConst.Length;
+            return Enum.Parse(type, enumConst[ind]);
         }
-        private void Create(out decimal result)
-        {
-            result =(decimal) rnd.NextDouble() / rnd.Next() * rnd.Next();
-        }
+            
     }
 }
--- FakerAPI/Program.cs
+++ FakerAPI/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace FakerAPI
 {
+    struct str {
+        int a;
+        int b;
+        public str( int sd) {
+            a = 10;
+            b = 1;
+        }
+    }
+
+    class a {
+        public int aaa;
+        public string b;
+        public string AAA { get; }
+        public a() {
+            aaa = 1;
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        private String val = "test";
+
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            a aa = new a();
+            var fld = typeof(a).GetProperty("AAA");
+            if (fld.GetValue(aa) == null) {
+                Console.WriteLine();
+            }
+            double h = default(double);
+            Console.WriteLine(fld.GetValue(aa));
         }
     }
 }
